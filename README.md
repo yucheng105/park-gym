eac64cae1430dc1a36c22378c0e6f091e467d1a6
yucheng105 <linyucheng105@gmail.com>
2026-06-13 14:37:58 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Best Time to Go to the Gym
---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+
+url = "https://winpoweryouthpark.com.tw/%E5%A0%B4%E9%A4%A8%E4%BB%8B%E7%B4%B9/%E5%81%A5%E8%BA%AB%E6%88%BF%E5%8D%80/%E5%81%A5%E8%BA%AB%E4%B8%AD%E5%BF%83/"
+
+response = requests.get(url, timeout=10)
+
+# 確認 request 成功
+response.raise_for_status()
+
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+counts = soup.select(".pcount")
+
+for item in counts:
+    if "體適能中心" in item.text:
+        count = item.select_one(".notice").text.strip()
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] 人數：{count}")
+
+        break
